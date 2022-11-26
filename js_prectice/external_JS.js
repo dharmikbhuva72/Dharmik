285967fa2b3dd6091eb946073e848440cd15f301
dharmikbhuva72 <109645263+dharmikbhuva72@users.noreply.github.com>
2022-11-26 10:45:42 +0530

    practice

---
+++ js_prectice/external_JS.js
@@ -0,0 +1,13 @@
+document.write("my self dharmik bhuva");
+
+console.log("hii satytam");
+
+document.getElementById('a').innerHTML="hello jay";
+
+alert("this is a alert box");
+
+confirm("are you sure..!");
+
+prompt("5+3 = ..?");
+
+print("");
